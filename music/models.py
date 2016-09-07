31eb47ca5e7b545f2f80a68bc3c943d5cc45f6df
Vishnu Gupta <guptavishnunitsk@gmail.com>
2016-09-08 01:28:20 +0530

    added new template and llogic for same

--- music/models.py
+++ music/models.py
@@ -10,7 +10,13 @@ class Album(models.Model):
     genre = models.CharField(max_length=100)
     album_logo = models.CharField(max_length=1000)
 
-class Songs(models.Model):
+    def __str__(self):
+        return self.artist + " created "+self.album_title
+
+class Song(models.Model):
     album = models.ForeignKey(Album,on_delete=models.CASCADE)# on deleting particular album all song corresponding to it will be deleted
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.song_title
--- music/views.py
+++ music/views.py
@@ -1,10 +1,29 @@
-#from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
-
-
-
+from .models import Album
+from django.http import Http404
+from django.template import loader
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('<h1>This is the Music app homepage</h1>')
+
+
+    #connect to database
+
+    all_album = Album.objects.all()
+    template = loader.get_template('music/index.html')
+    context = {
+         'all_album':all_album
+    }
+    return HttpResponse(template.render(context,request))
+
+
+def detail(request,album_id):
+    ''' try:
+         album = Album.objects.get(pk=album_id)
+     except Album.DoesNotExist:
+         raise Http404("Album does not exit")'''  # shortcut is below
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {'album': album})
+
